7b5939474dc8edbdae59ae730cbd76c3d600e5e8
sarastefanova <sara_stefanova@hotmail.com>
2019-05-06 13:09:27 +0200

    Add project files.

---
+++ PlaneCrash/HomePage.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace PlaneCrash
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnInstructions = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.Crimson;
+            this.btnNewGame.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnNewGame.Location = new System.Drawing.Point(691, 327);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(131, 32);
+            this.btnNewGame.TabIndex = 3;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.BtnNewGame_Click_1);
+            // 
+            // btnInstructions
+            // 
+            this.btnInstructions.BackColor = System.Drawing.Color.Crimson;
+            this.btnInstructions.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.btnInstructions.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInstructions.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnInstructions.Location = new System.Drawing.Point(691, 365);
+            this.btnInstructions.Name = "btnInstructions";
+            this.btnInstructions.Size = new System.Drawing.Size(131, 32);
+            this.btnInstructions.TabIndex = 4;
+            this.btnInstructions.Text = "How to play";
+            this.btnInstructions.UseVisualStyleBackColor = false;
+            this.btnInstructions.Click += new System.EventHandler(this.BtnInstructions_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.BackColor = System.Drawing.Color.Crimson;
+            this.btnQuit.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.btnQuit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnQuit.Location = new System.Drawing.Point(691, 403);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(131, 32);
+            this.btnQuit.TabIndex = 5;
+            this.btnQuit.Text = "Quit game";
+            this.btnQuit.UseVisualStyleBackColor = false;
+            this.btnQuit.Click += new System.EventHandler(this.BtnQuit_Click);
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PlaneCrash.Properties.Resources.Cover;
+            this.ClientSize = new System.Drawing.Size(937, 488);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnInstructions);
+            this.Controls.Add(this.btnNewGame);
+            this.Name = "HomePage";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnInstructions;
+        private System.Windows.Forms.Button btnQuit;
+    }
+}
+
---
+++ PlaneCrash/HomePage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlaneCrash
+{
+    public partial class HomePage : Form
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+        }
+
+
+        private void BtnNewGame_Click_1(object sender, EventArgs e)
+        {
+            NewGame newGame = new NewGame();
+            newGame.ShowDialog();
+        }
+
+        private void BtnInstructions_Click(object sender, EventArgs e)
+        {
+            HowToPlay howToPlay = new HowToPlay();
+            howToPlay.ShowDialog();
+        }
+
+        private void BtnQuit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Quit game, are you sure?", "Quit game", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
